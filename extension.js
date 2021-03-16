3ca44ba081aba8df2231d40feb150e93da31279d
Mathieu Fay <mfay@free.fr>
2021-03-16 20:01:56 +0100

    Getion du mot de passe

--- extension.js
+++ extension.js
@@ -6,7 +6,7 @@ const path   = require('path') ;
 const fs     = require('fs') ;
 const yaml   = require('js-yaml') ;	
 const os     = require('os') ;  
-  
+
 // ==============================
 //    CCC   L       OOO    GGGG
 //   C   C  L      O   O  G
@@ -20,7 +20,136 @@ function clog(...tb) {
         console.log(tb) ;
 }   }
 
+  
+// ======================================================================
+//    CCC    OOO   DDDD    AAA    GGGG  EEEEE       PPPP   W   W  DDDD
+//   C   C  O   O  D   D  A   A  G      E           P   P  W   W  D   D
+//   C      O   O  D   D  AAAAA  G  GG  EEEE        PPPP   W W W  D   D
+//   C   C  O   O  D   D  A   A  G   G  E           P      W W W  D   D
+//    CCC    OOO   DDDD   A   A   GGG   EEEEE       P       W W   DDDD
+// ======================================================================
+//* Encodage et décodage du password  
+// * * * Encodage utf-8 * * *
+function encodPW(t) {
+	let res = '' ;
+	let prc = 42 ;
+	for(let i in t) {
+		let c   = Number(t.charCodeAt(i)) ; 
+		let cc  = c + prc ; 
+		prc = c ; 
+		if (cc > 255) { cc = cc - 255 ; }
+		res += cc.toString(16) 
+	}
+	return res
+}
+// * * * Décodage utf-8 * * *
+function decodPW(t) {
+	let res = '' ;
+	let prc = 42 ;
+	for(let i = 0; i < t.length; i+=2) {
+		let v = parseInt(t.substr(i, 2), 16) ; 
+		prc = v - prc ;
+		if (prc < 0) {prc = prc + 255 ; } 
+		res += String.fromCharCode(prc) ; 
+	}
+	return res ;
+}
 
+// ==================================================================================================
+//   RRRR   EEEEE   CCC   U   U  PPPP        PPPP    AAA    SSSS   SSSS  W   W   OOO   RRRR   DDDD
+//   R   R  E      C   C  U   U  P   P       P   P  A   A  S      S      W   W  O   O  R   R  D   D
+//   RRRR   EEEE   C      U   U  PPPP        PPPP   AAAAA   SSS    SSS   W W W  O   O  RRRR   D   D
+//   R  R   E      C   C  U   U  P           P      A   A      S      S  W W W  O   O  R  R   D   D
+//   R   R  EEEEE   CCC    UUU   P           P      A   A  SSSS   SSSS    W W    OOO   R   R  DDDD
+// ==================================================================================================
+// a partir du compte, récupération du password (ou demande du password)
+let recupPassword = async function(adresse, fichierYaml) {
+	let fichierPassword = path.join(path.dirname(fichierYaml), 'boFTP.password') ;
+	let objPassword = {} ;
+	let password = '' ;
+	if (fs.existsSync(fichierPassword)) {	
+		let t = fs.readFileSync(fichierPassword, 'latin1') ;
+		objPassword = JSON.parse(t);
+		password = objPassword[adresse] ;
+	} 
+	if (password == undefined || password == null || password == '') {
+		password = '' ;
+		let pass = await vscode.window.showInputBox({ placeHolder: 'Mot de passe', prompt: 'Saisissez votre Mot de Passe Mainframe', password: true});
+		if (pass && pass != '') {
+		     password = pass ;
+			 // * * * sauvegarde fichier password * * *
+			 objPassword[adresse] = encodPW(password) ;
+			 let cont = JSON.stringify(objPassword) ;
+			 fs.writeFileSync(fichierPassword, cont, 'latin1') ;
+		}
+	} else {
+		password = decodPW(password) ;
+	}
+	return password ;
+}  
+
+  
+// ==================================================================================================
+//   RRRR   EEEEE   CCC   H   H  EEEEE  RRRR    CCC   H   H  EEEEE       Y   Y   AAA   M   M  L
+//   R   R  E      C   C  H   H  E      R   R  C   C  H   H  E            Y Y   A   A  MM MM  L
+//   RRRR   EEEE   C      HHHHH  EEEE   RRRR   C      HHHHH  EEEE          Y    AAAAA  M M M  L
+//   R  R   E      C   C  H   H  E      R  R   C   C  H   H  E             Y    A   A  M   M  L
+//   R   R  EEEEE   CCC   H   H  EEEEE  R   R   CCC   H   H  EEEEE         Y    A   A  M   M  LLLLL
+// ==================================================================================================
+let lectureYAML = async function(dirFich) {
+
+	// * * * Recherche du fichier YAML et alim sous-dossier ftp * * *
+	let fi     = dirFich ;
+	let fp     = '' ;
+	let fiYaml = '' ;
+	let dossierFtp = '' ;
+	while (fi.length > 5 && fi != fp && fi != undefined && fiYaml == '') {
+		let t = path.join(fi, 'boFTP.yaml') ;
+		if (fs.existsSync(t)) {	
+			fiYaml = t ;
+			dossierFtp = dirFich.substr(path.dirname(fiYaml).length + 1) ;
+		} else {
+			fi = path.dirname(fi) ;
+		}
+	}
+	dossierFtp = dossierFtp.replace(/\\/g, "/") ;
+	// * * * controle présence YAML * * *
+	if (fiYaml == '') {
+		vscode.window.showErrorMessage('boFTP - désolé : fichier "boFTP.yaml" non trouvé !');
+		clog('Yaml non trouvé !') ;
+		return { retour: false };
+	}
+
+	// * * * Ouverture du fichier YAML * * *
+	let fiYamlCont = fs.readFileSync(fiYaml) ;
+	let connexionList = yaml.safeLoad(fiYamlCont) ; 
+	let connActif = connexionList.actif ; clog(connActif) ;
+	if (connActif == undefined) {
+		vscode.window.showErrorMessage('boFTP - manque le paramètre "actif" dans le fichier "boFTP.yaml" !');
+		clog('connActif undefined') ;
+		return { retour: false };
+	}
+	let connex = connexionList.connexions[connActif] ;
+	if (connex == undefined || connex.adresse == undefined || connex.user == undefined) {
+		vscode.window.showErrorMessage('boFTP - fichier "boFTP.yaml" non conforme !');
+		clog('connex', connex) ;
+		return { retour: false };
+	}
+	if (connex.dossier == undefined) {connex.dossier = '' ; }
+
+	// * * * Récupération du password * * *
+	let password = await recupPassword(connex.adresse + '-' + connActif, fiYaml, connexionList.actif) ;
+	if (password == '') {
+		vscode.window.showErrorMessage('boFTP - manque le mot de passe !');
+		clog('password innexistant') ;
+		return { retour: false }
+	}
+
+	// * * * Retour * * *
+	clog('connex', connex) ;
+	return { retour: true, connex: connex, dossierFtp: dossierFtp, fichierYaml: fiYaml, password: password } ;
+
+}
   
 // ===========================================================================================
 //    AAA   FFFFF  FFFFF  IIIII   CCC   H   H   AAA    GGGG  EEEEE       W   W  EEEEE  BBBB
@@ -75,44 +204,12 @@ let moduleFTP = async function(mode='trsf') {
 	let nomFich     = path.basename(adrFich) ; 
 	let dirFich     = path.dirname(adrFich) ;
 
-	// * * * Recherche du fichier YAML et alim sous-dossier ftp * * *
-	let fi     = dirFich ;
-	let fp     = '' ;
-	let fiYaml = '' ;
-	let dossierFtp = '' ;
-	while (fi.length > 5 && fi != fp && fi != undefined && fiYaml == '') {
-		let t = path.join(fi, 'boFTP.yaml') ;
-		if (fs.existsSync(t)) {	
-			fiYaml = t ;
-			dossierFtp = dirFich.substr(path.dirname(fiYaml).length + 1) ;
-		} else {
-			fi = path.dirname(fi) ;
-		}
-	}
-	dossierFtp = dossierFtp.replace(/\\/g, "/") ;
-	// controle présence YAML
-	if (fiYaml == '') {
-		vscode.window.showErrorMessage('boFTP - désolé : fichier "boFTP.yaml" non trouvé !');
-		clog('Yaml non trouvé !') ;
-		return ;
-	}
-
-	// * * * Ouverture du fichier YAML * * *
-	let fiYamlCont = fs.readFileSync(fiYaml, 'utf8') ;
-	let connexionList = yaml.safeLoad(fiYamlCont) ; 
-	let connActif = connexionList.actif ; clog(connActif) ;
-	if (connActif == undefined) {
-		vscode.window.showErrorMessage('boFTP - manque le paramètre "actif" dans le fichier "boFTP.yaml" !');
-		clog('connActif undefined') ;
-		return ;
-	}
-	let connex = connexionList.connexions[connActif] ;
-	if (connex == undefined || connex.adresse == undefined || connex.user == undefined || connex.dossier == undefined) {
-		vscode.window.showErrorMessage('boFTP - fichier "boFTP.yaml" non conforme !');
-		clog('connex', connex) ;
-		return ;
-	}
-	clog('connex', connex) ;
+	// * * * Lecture du fichier YAML
+	let lectYaml = await lectureYAML(dirFich) ;
+	if (lectYaml.retour == false) { return ; }
+	let connex     = lectYaml.connex ;
+	let dossierFtp = lectYaml.dossierFtp ;
+	let password   = lectYaml.password ;
 
 	// * * * Paramètre fichier * * *
 	let tmpFTP     = path.join(os.tmpdir(),'cmdftp.prm') ; 
@@ -120,7 +217,7 @@ let moduleFTP = async function(mode='trsf') {
 	// * * * Lancement FTP * * *
 	let cmdFTP =
 			  "open " + connex.adresse + " \n" +
-			  "user " + connex.user + ' ' + connex.password + " \n" ;
+			  "user " + connex.user + ' ' + password + " \n" ;
 	if (connex.dossier != '') {
 		cmdFTP +=
 			  "cd \"" + connex.dossier + "\" \n" ;
